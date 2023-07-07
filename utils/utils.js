a5ca3bebb9c681dfd6826d959562d8f0ae73bd31
SpielerEinzig <SpielerEinzig@gmail.com>
2023-07-07 19:49:50 +0100

    fixed send email service in utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const axios = require('axios');
 const redis = require("redis");
+const nodemailer = require('nodemailer');
 
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 const REDIS_HOST = process.env.REDIS_HOST;
@@ -8,8 +9,8 @@ const REDIS_HOST = process.env.REDIS_HOST;
 const client = redis.createClient({
   password: REDIS_PASSWORD,
   socket: {
-      host: REDIS_HOST,
-      port: 11813
+    host: REDIS_HOST,
+    port: 11813
   }
 });
 
@@ -19,81 +20,70 @@ async function checkURL(req, res, next) {
     const response = await axios.head(url);
     if (response.status >= 200 && response.status < 400) {
       next();
-      
-    } else 
-    {
-    return res.status(400).json({message: "Broken URL!"})
+
+    } else {
+      return res.status(400).json({ message: "Broken URL!" })
     }
   } catch (error) {
 
     console.log(error);
-    return res.status(500).json({message: "An error occured!"});
-    
+    return res.status(500).json({ message: "An error occured!" });
+
   }
 }
 
-  function generateShortURL() {
-    var length = Math.floor(Math.random() * 6) + 2; // Generate a random length between 2 and 7
-    var alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    var result = '';
-  
-    for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * alphanumeric.length);
-      result += alphanumeric.charAt(randomIndex);
+function generateShortURL() {
+  var length = Math.floor(Math.random() * 6) + 2; // Generate a random length between 2 and 7
+  var alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * alphanumeric.length);
+    result += alphanumeric.charAt(randomIndex);
+  }
+  return result;
+
+
+}
+
+function cache(req, res, next) {
+  const key = "__express__" + req.originalUrl || req.url;
+
+  client.get(key).then(reply => {
+
+    if (reply) {
+      res.send(JSON.parse(reply));
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        //expire in 1 min
+        client.set(key, JSON.stringify(body), { 'EX': 60 });
+        res.sendResponse(body);
+      };
+      next();
     }
-    return result;
+  }).catch(err => {
+    console.log(err);
+    res.status(500).send(err)
+  });
+}
 
+async function sendEmail(receiverMail, subject, htmlBody) {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {user: process.env.MAIL_USERNAME, pass: process.env.MAIL_PASSWORD}
+  });
 
-  }
+  let info = await transporter.sendMail({
+    from: process.env.MAIL_USERNAME,
+    to: receiverMail,
+    subject: subject,
+    html: htmlBody,
+  });
 
-  function cache(req, res, next) {
-    const key = "__express__" + req.originalUrl || req.url;
-  
-    client.get(key).then(reply => {
-      
-      if (reply) {
-        res.send(JSON.parse(reply));
-      } else {
-        res.sendResponse = res.send;
-        res.send = (body) => {
-          //expire in 1 min
-          client.set(key, JSON.stringify(body), {'EX':60});
-          res.sendResponse(body);
-        };
-        next();
-      }
-    }).catch(err=>{
-      console.log(err);
-      res.status(500).send(err)
-    });
-  }
-  
+  console.log(info.messageId); 
+}
 
-  async function sendEmail() {
-    
-    transporter.verify(function(error, success) {
-      if (error) {
-            console.log(error);
-      } else {
-            console.log('Server is ready to take our messages');
-      }
-    });
-    
-    await  utils.transporter.sendMail(
-      {
-        from: "rwandagloria@gmail.com",
-        to: body.email,
-        subject: 'Nodemailer Project',
-       html: `<h1>Hello Gloria! </h1> <h2> This is your link: ${confirmationURL}. It expires in two hours. `
-      }, function(err, data) {
-          if (err) {
-            console.log("Error " + err);
-          } else {
-            console.log("Email sent successfully");
-          }
-        });
-      return res.status(200).send(`We have sent an email to ${body.email} to confim the validity of the email. After receiving the email, follow the link provided to complete registration.`);
-      
-  }
-
-  module.exports = { checkURL, generateShortURL, cache, client }
+module.exports = { checkURL, generateShortURL, cache, sendEmail, client }

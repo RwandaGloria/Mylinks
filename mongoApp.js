75316ec90a8471b74f8b3bb0d1bdc97c3f7bacf1
SpielerEinzig <SpielerEinzig@gmail.com>
2023-07-10 02:34:32 +0100

    added swagger config to mongoApp.js file

--- mongoApp.js
+++ mongoApp.js
@@ -15,24 +15,45 @@ mongoose.connect(process.env.MONGO_URL).then((result) => {
     app.listen(8099);
     console.log("Connected to DB");
 
-    utils.client.connect().then(()=> {
+    utils.client.connect().then(() => {
         console.log('redis is connected')
-      })
+    })
 }).catch((error) => {
     console.log(error);
 });
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 app.use(mongoRoutes);
 
+//Swagger definitions
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'URL Shortener(My links)',
+        description: "This is Altschool capstone project",
+        version: '1.0.0',
+    },
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/mongo_routes.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+const swaggerUi = require('swagger-ui-express');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 //Error message for wrong endpoint
 app.use((req, res) => {
-    const message = ({"message": "You tapped the wrong endpoint"});
+    const message = ({ "message": "You tapped the wrong endpoint" });
 
     res.json(message);
 
